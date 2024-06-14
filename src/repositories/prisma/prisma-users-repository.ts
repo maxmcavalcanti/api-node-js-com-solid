2c4d097a956bc2a556cb515b4e301b62009669d7
Maximiliano Cavalcanti <max@neoep.com.br>
2024-06-14 15:20:32 -0300

    prisma checkins repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 export class PrismaUsersRepository implements UsersRepository {
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({ where: { email } })
     return user
