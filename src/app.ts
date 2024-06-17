7f782491b2037c59bdb87a635d8fbfc617570317
Maximiliano Cavalcanti <max@neoep.com.br>
2024-06-17 12:28:48 -0300

    refresh token controller and test

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,22 @@ import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import { gymRoutes } from "./http/controllers/gyms/route";
+import fastifyCookie from "@fastify/cookie";
 import { userRoutes } from "./http/controllers/users/routes";
 import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 
 export const app = fastify()
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m',
+  }
 })
-
+app.register(fastifyCookie)
 app.register(userRoutes)
 app.register(gymRoutes)
 app.register(checkInsRoutes)
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -16,15 +16,28 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
     const authenticateUseCase = makeAuthenticateUseCase()
 
     const { user } = await authenticateUseCase.execute({ email, password })
-
     const token = await reply.jwtSign({}, {
       sign: {
         sub: user.id
       }
     })
-    return reply.status(200).send({
-      token
+
+    const refreshToken = await reply.jwtSign({}, {
+      sign: {
+        sub: user.id,
+        expiresIn: '7d'
+      }
     })
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true
+      })
+      .status(200)
+      .send({ token })
 
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
