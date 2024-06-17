0c44bd92c9876537c20a24724415b3fd4dc51685
Maximiliano Cavalcanti <max@neoep.com.br>
2024-06-17 10:13:08 -0300

    environment setup e2e tets

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,18 @@
 
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case"
 import { FastifyReply, FastifyRequest } from "fastify"
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  await request.jwtVerify()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub
+  })
 
-  return reply.status(201).send()
+  return reply.status(201).send({
+    user: {
+      ...user,
+      password_hash: undefined
+    }
+  })
 }
