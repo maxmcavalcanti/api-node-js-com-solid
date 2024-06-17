c4ce9c106c29f60b01ff74c5ac200e6c8634964f
Maximiliano Cavalcanti <max@neoep.com.br>
2024-06-17 10:51:57 -0300

    authentication e2e test

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,5 +1,35 @@
-import { expect, test } from "vitest"
+import { afterAll, beforeAll, describe, expect, test } from "vitest"
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {
-  expect(1).toBe(1)
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('should be able to authenticate', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'pHxP8@example.com',
+        password: '123456'
+      })
+
+    const response = await request(app.server)
+      .post('/sessions')
+      .send(
+        {
+          email: 'pHxP8@example.com',
+          password: '123456'
+        })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.token).toEqual(expect.any(String))
+  })
 })
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,25 @@
-import { expect, test } from "vitest"
+import { afterAll, beforeAll, describe, expect, test } from "vitest"
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {
-  expect(1).toBe(1)
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test('should be able to register', async () => {
+    const response = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'John Doe',
+        email: 'pHxP8@example.com',
+        password: '123456'
+      })
+    expect(response.statusCode).toEqual(201)
+  })
 })
